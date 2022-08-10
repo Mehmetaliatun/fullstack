158eb729370962dcb17b2a020be0533745f78eb8
Atun <mali1996ua@gmail.com>
2022-08-10 20:25:48 +0300

    all notes and projects about fullstack course

---
+++ Fullstack/JavaScript/Lesson2/App.js
@@ -0,0 +1,141 @@
+// console.log('App js Running')
+
+// let a = 5;
+// console.log(a);
+
+// {
+//     let a = 3;
+//     console.log(a);
+// }
+
+
+// console.log(a);
+
+// console.log(typeof 0)
+// console.log(typeof 3.14)
+// console.log(typeof 'Hello')
+// console.log(typeof (8 > 5))
+// console.log(typeof 8 > 5)
+// console.log(typeof '')
+
+// -yukarıda typeof ile çeşitleri gösteriyor
+
+// let b = +prompt ('Bir şey giriniz')
+
+// console.log(b, typeof b)
+
+// -string olan sayı ifadesini
+// başına + ekleyip number tipine
+// parseInt(stringVar) ile integer number a
+// parseFloat(stringVar) ile kesirli sayı
+// elde ederiz
+
+// console.log(typeof typeof 42) 42 'number' değerini alıyor diğer typeof ile birlikte string oluyor.
+
+// let age;
+
+// console.log(age);
+
+// let myAge = 40;
+// console.log(myAge);
+
+
+// console.log('hello');
+
+// let a = null;
+
+// console.log(typeof typeof a);
+
+// let x;
+
+// do{
+//     x = prompt('enter a Number :');
+// }while ( isNaN(x));
+
+// console.log(x, x*x);
+
+
+// console.log('12' === 12);
+// console.log('12' * '12')
+
+// let g = 0.1 + 0.2;
+// console.log(g)
+// console.log(g.toFixed(2));
+
+// let h = 1_000_000_000
+// console.log(h)
+
+
+// let x = 7;
+// console.log(x);
+// let y = 33.33;
+// console.log(y);
+
+
+
+// -String-
+
+// `` = alt+96 ile yapılıyor
+
+// \ = alt+92 ile yapılıyor veya alt+0'ın sağındaki tuş
+
+// let i = 'This a string';
+// let j = "This a string";
+// let k = `This a string`;
+
+// console.log(typeof i)
+// console.log(typeof j)
+// console.log(typeof k)
+
+// let userName = 'Musa';
+// console.log(userName.length)
+
+// console.log('Merhaba', userName)
+
+// console.log(`Merhaba ${userName}`)
+// console.log(`2 + 3 = ${'2'+'3'}`)
+
+// console.log('3' - 2)
+
+
+// -Booleans- true/false
+
+// let l = 8>5;
+// let m = 42 == 35;
+// console.log(`l = ${l}`)
+// console.log(`m = ${m}`)
+
+// -objects-
+
+// let n = ``
+// let o = ' '
+// let p = 0
+
+// console.log(Boolean(n))
+// console.log(Boolean(o))
+// console.log(Boolean(p))
+// console.log(Boolean(3 > 5))
+// console.log(Boolean(3 > 2))
+
+// const myCar = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1965,
+//     color: 'Black'
+// }
+
+// console.log(myCar)
+
+// myCar.color = 'green'
+
+// console.log(myCar)
+
+// myCar.sunRoof = false;
+
+// console.log(myCar)
+
+// myCar.age = function(current){
+//     console.log(current - this.year)
+// }
+
+// myCar.age(2022)
