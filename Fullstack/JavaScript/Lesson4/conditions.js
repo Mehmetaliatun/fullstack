b340e23c8268cfda5057a3d26959d00975f3fc77
Atun <mali1996ua@gmail.com>
2022-08-16 21:32:58 +0300

    end comb.

---
+++ Fullstack/JavaScript/Lesson4/conditions.js
@@ -0,0 +1,44 @@
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+//**************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+// const number = prompt("Enter a number:");
+
+//     if (number > 0 ) {
+//     console.log(`${number} is positive.`);
+// }   else if (number == 0) 
+//     console.log(`${number} equal to 0.`);
+//     else {
+//     console.log(`${number} is negative.`);
+// }
+//     console.log(number, typeof number);
+// -yukarıyı düzelt
+
+
+    // if (number > 0) {
+//     console.log(`${number} pozitiftir.`);
+//   } else if (number == 0) {
+//     console.log(`${number}'a esittir.`);
+//   } else {
+//     console.log(`${number} negatiftir.`);
+//   }
+//   console.log(number, typeof number);
+
+
+// const n1 = prompt("number1:")
+// const n2 = prompt("number2:")
+// const n3 = prompt("number3:")
+
+// if(n1 >= n2 && n1 >= n3){
+//     console.log(`${n1} is the biggest`);
+// } 
+// else if(n2 >= n1 && n2 >= n3){
+//     console.log(`${n2} is the biggest`);
+// } 
+// else if(n3 >= n2 && n3 >= n1){
+//     console.log(`${n3} is the biggest`);
+// }
