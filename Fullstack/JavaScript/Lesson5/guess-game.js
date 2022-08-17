923ead117264beafe7cd52495c20438957be5cb4
Atun <mali1996ua@gmail.com>
2022-08-17 22:45:19 +0300

    do-while-end

---
+++ Fullstack/JavaScript/Lesson5/guess-game.js
@@ -0,0 +1,8 @@
+//? Guess Game
+
+//* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
+
+const guess = 5;
+console.log("Guess random number:");
+
+while ( )
