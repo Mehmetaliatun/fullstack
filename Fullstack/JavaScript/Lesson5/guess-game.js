a7711720a7d59a96b9e33abaab1d9e3d02bb5a60
Atun <mali1996ua@gmail.com>
2022-08-17 22:54:44 +0300

    guess-game

--- Fullstack/JavaScript/Lesson5/guess-game.js
+++ Fullstack/JavaScript/Lesson5/guess-game.js
@@ -2,7 +2,20 @@
 
 //* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
 
-const guess = 5;
-console.log("Guess random number:");
+//* Psuedo Code
 
-while ( )
+//* 1- hak =5
+//* 2- Raskele sayı tut (0-100)
+//* 3- Kullanıcan tahmin iste (tahmın)
+//* 4- hak=hak-1
+//* 5- eğer (tahmin =?rasgele)
+//*        print(Tebrikler Bildiniz)
+//*    değilse (tahmin < rasgele)
+//*        print(Arttır)
+//*    değilse(tahmin > rastgele)
+//*        print(Azalt)
+//* 6- eğer (hak > 0 )
+//*       goto stage-3
+//*   değilse
+//*       print(üzgünüz)
+
