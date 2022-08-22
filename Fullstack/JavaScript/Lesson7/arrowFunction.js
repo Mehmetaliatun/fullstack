e696632318c5e98ce13a26d4689ad50f0002f08c
Atun <mali1996ua@gmail.com>
2022-08-22 20:27:11 +0300

    arrowFunc

---
+++ Fullstack/JavaScript/Lesson7/arrowFunction.js
@@ -0,0 +1,13 @@
+//** -----Arrow------ */
+
+// let vkiHesaplama = (kilo, boy) => kilo / (boy * boy);
+// console.log(vkiHesaplama(64, 1.74));
+
+// const topla = (a, b) => a + b;
+
+// console.log(topla(2, 3));
+
+// const usAl = (t, u) => t ** u;
+// console.log(usAl(2, 3));
+
+const calculate = (x, y, islem) => {};
