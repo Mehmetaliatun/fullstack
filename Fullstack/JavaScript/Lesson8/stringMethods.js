90a739185a79952cbdcb9735220a6576a4c35dca
Atun <mali1996ua@gmail.com>
2022-08-24 20:36:43 +0300

    concat

--- Fullstack/JavaScript/Lesson8/stringMethods.js
+++ Fullstack/JavaScript/Lesson8/stringMethods.js
@@ -7,8 +7,35 @@ console.log("*** STRING METHODS ***");
 //! Klasik yontem (String Literals) ile tanimlanirsa bu string bir
 //! primitive degiskendir.
 
-//! tek değer tutan primitive
+//! tek deger tutan primitive
 //! bolean, integer, string, undeifned number,primitive veri tipi
 //! non primitve ise object
 const str1 = "Clarusway";
 //Char => C, l, a,....
+const str2 = " Hello";
+const str3 = " Full Stack";
+
+console.log(str1, typeof str1);
+
+//! Non-primitive String tanimlama
+const str4 = new String("Hello FS");
+console.log(typeof str4);
+
+//?---------------------------------------
+console.log(str1.toLocaleLowerCase());
+
+//* Normalde primitive veri tiplerinin property veya metodu olmaz.
+//* Ancak,Javascript otomatik olarak primitive String'ler için object wrapper oluşturur.
+//* Bu sayede, String nesnelerine ait olan fonksiyonlar veya özellikleri
+//* primitive string'ler ile kullanılabilir hale gelir.
+//* Sonrasında oluşan bu object bellekten otomatik olarak kaldırılır.
+
+//? Ek Açıklama için : https://javascript.info/primitives-methods
+
+const sayi = 5;
+const str5 = str1 + str2 + str3 + sayi;
+console.log(str5, typeof str5);
+
+//* ---------------------------------------------------
+//*  concat() - immutable
+//* ---------------------------------------------------
