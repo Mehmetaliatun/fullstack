dd3157593d287f8e1b531056be5f4ea2a34e7aae
Atun <mali1996ua@gmail.com>
2022-08-24 20:12:48 +0300

    string methods

---
+++ Fullstack/JavaScript/Lesson8/stringMethods.js
@@ -0,0 +1,14 @@
+// ?======================================================
+// ?                     STRING METOTLARI
+// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// ?======================================================
+
+console.log("*** STRING METHODS ***");
+//! Klasik yontem (String Literals) ile tanimlanirsa bu string bir
+//! primitive degiskendir.
+
+//! tek değer tutan primitive
+//! bolean, integer, string, undeifned number,primitive veri tipi
+//! non primitve ise object
+const str1 = "Clarusway";
+//Char => C, l, a,....
