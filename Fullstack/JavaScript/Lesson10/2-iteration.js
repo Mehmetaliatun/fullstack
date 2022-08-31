b99dfec6057e24c34898a6f3d3cd4194ea594f02
Atun <mali1996ua@gmail.com>
2022-08-31 22:34:09 +0300

    loops

---
+++ Fullstack/JavaScript/Lesson10/2-iteration.js
@@ -0,0 +1,47 @@
+//* ======================================================
+//*                   FOREACH METHOD
+//* ======================================================
+
+console.log("****** ITERATION **********");
+
+//
+//?-------------- ÖRNEK -------------------
+//?  Dizideki herbir fiyati konsola bastiriniz.
+
+const prices = [100, 250, 50, 89];
+// prices.forEach((p) => console.log(p));
+
+//? ORNEK: prices dizisindekilerin toplamini konsola yazdiriniz */
+let sum = 0;
+prices.forEach((price) => (sum += price));
+console.log("SUM:", sum);
+
+//! NOT: forEach metodu void metottur.(Herhangi bir deger dondurmez)
+console.log(prices.forEach((price) => (sum += price))); //! undefined
+
+//?-------------- ÖRNEK -------------------
+//? prices dizisindeki her bir ara toplam degerini
+//? konsola bastiriniz. Ayrica her bir fiyata %10 zam yapiniz.
+
+let total = 0;
+prices.forEach((price, index, arr) => {
+  total += price;
+  console.log(`${index + 1}.iteration:${total}`);
+  arr[index];
+});
+
+//* ======================================================
+//*                       MAP METHOD
+//* ======================================================
+
+//* ======================================================
+//*                CHAINING (PIPELINE)
+//* ======================================================
+
+//* ======================================================
+//*                 FILTER METHOD
+//* ======================================================
+
+//* ======================================================
+//*                 REDUCE METHOD
+//* ======================================================
