3f9284b1b4132d5cf259629fd11f29c31a2c6588
Atun <mali1996ua@gmail.com>
2022-08-31 22:50:38 +0300

    chaining

--- Fullstack/JavaScript/Lesson10/2-iteration.js
+++ Fullstack/JavaScript/Lesson10/2-iteration.js
@@ -27,17 +27,37 @@ let total = 0;
 prices.forEach((price, index, arr) => {
   total += price;
   console.log(`${index + 1}.iteration:${total}`);
-  arr[index];
+  arr[index] = Math.trunc(price - 1.1);
 });
-
+console.log(prices);
 //* ======================================================
 //*                       MAP METHOD
 //* ======================================================
+
+//?-------------- ÖRNEK -------------------
+//? Bir dizideki tüm isimleri BÜYÜK harfe dönüştüren uygulamayı yazınız.
+let names = ["Mustafa", "Murat", "Ahmet", "Mustafa", "Ayşe", "canan"];
+
+const bigNames = names.map((name) => name.toUpperCase());
+console.log(bigNames, names);
+console.log(names.map((name) => name.toUpperCase()));
 
 //* ======================================================
 //*                CHAINING (PIPELINE)
 //* ======================================================
 
+//?-------------- ÖRNEK -------------------
+//? tlFiyatlar dizisindeki rakamlarin Euro ve dolar
+//? karsiliklarini hesaplatarak yeni dizelere kaydediniz
+
+const euro = 18.23;
+const dolar = 18.19;
+const tlPrices = [100, 150, 100, 50, 80];
+
+const euroPrices = tlPrices.map((tl) => (tl / euro).toPrecision(3));
+console.log(euroPrices);
+const dolarPrices = tlPrices.map((tl) => (tl / dolar).toPrecision(2));
+console.log(dolarPrices);
 //* ======================================================
 //*                 FILTER METHOD
 //* ======================================================
