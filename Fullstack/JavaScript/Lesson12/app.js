0d5b4ef145236f56c822c9152ee231dc1e5010cc
Atun <mali1996ua@gmail.com>
2022-09-07 20:34:15 +0300

    new generation operators

---
+++ Fullstack/JavaScript/Lesson12/app.js
@@ -0,0 +1,15 @@
+// let daltones = ["joe", "Jack", "Willam", "Averell"];
+// daltones.shift();
+// daltones.pop();
+// console.log(daltones);
+
+// let daltones = ["joe", "Jack", "Willam", "Averell"];
+// for (let i = 0; i < daltones.length; i++) {
+//   if (i == 1) {
+//     continue;
+//   }
+//   console.log(daltones[i]);
+// }
+
+let avengers = ["Iron Man", "Captain America", "Black Widow", "Hulk"];
+avengers.splice(2, 1, "Thor", "Hawkeye");
