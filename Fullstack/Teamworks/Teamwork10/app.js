4647b8562534646fbb80007b54cfa16304c3bfeb
Atun <mali1996ua@gmail.com>
2022-09-09 20:53:48 +0300

    teamwork

---
+++ Fullstack/Teamworks/Teamwork10/app.js
@@ -0,0 +1,9 @@
+// let fullStack = {
+//   languages: ["JavaScript", "React", "HTML"],
+//   jira: true,
+//   gitHub: true,
+//   difficulty: 8,
+// };
+// for (let i = 0; i < fullStack.languages.length; i++) {
+//   console.log(fullStack.languages[i]);
+// }
