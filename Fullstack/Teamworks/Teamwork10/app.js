d4502730507ce349c1f2156403b6b04038ab9b2b
Atun <mali1996ua@gmail.com>
2022-09-09 20:55:45 +0300

    third example

--- Fullstack/Teamworks/Teamwork10/app.js
+++ Fullstack/Teamworks/Teamwork10/app.js
@@ -8,12 +8,23 @@
 //   console.log(fullStack.languages[i]);
 // }
 
-let fullStack = {
-  languages: ["JavaScript", "React", "HTML"],
-  jira: true,
-  gitHub: true,
-  difficulty: 8,
+// let fullStack = {
+//   languages: ["JavaScript", "React", "HTML"],
+//   jira: true,
+//   gitHub: true,
+//   difficulty: 8,
+// };
+// for (let key in fullStack) {
+//   console.log(key);
+// }
+
+const myCar = {
+  make: "ford",
+  model: "Mustang",
+  year: 1965,
+  color: "Black",
 };
-for (let key in fullStack) {
-  console.log(key);
-}
+myCar.age = function (current) {
+  console.log(current - this.year);
+};
+myCar.age(2022);
