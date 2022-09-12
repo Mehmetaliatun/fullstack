feceec60c504f1068652ab899de8d94e9126be4e
Atun <mali1996ua@gmail.com>
2022-09-12 22:27:24 +0300

    selectors

--- Fullstack/JavaScript/Lesson14/2-event-example/app.js
+++ Fullstack/JavaScript/Lesson14/2-event-example/app.js
@@ -1,1 +1,10 @@
 console.log("**** app.js *******");
+
+//? Selectors
+const langInput = document.querySelector(".langInput");
+const addBtn = document.querySelector("#addBtn");
+const deletebtn = document.querySelector("#deletebtn");
+const langList = document.getElementById("langList");
+
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
