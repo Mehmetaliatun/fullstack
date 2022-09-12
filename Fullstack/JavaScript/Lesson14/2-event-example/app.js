188b3a83a5fd7bea478ce60b1934cd8827e4c78a
Atun <mali1996ua@gmail.com>
2022-09-12 22:41:09 +0300

    delete button

--- Fullstack/JavaScript/Lesson14/2-event-example/app.js
+++ Fullstack/JavaScript/Lesson14/2-event-example/app.js
@@ -1,10 +1,33 @@
 console.log("**** app.js *******");
 
 //? Selectors
-const langInput = document.querySelector(".langInput");
-const addBtn = document.querySelector("#addBtn");
-const deletebtn = document.querySelector("#deletebtn");
-const langList = document.getElementById("langList");
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
 
 const newUl = document.createElement("ul");
 langList.appendChild(newUl);
+
+//? addBtn event handler
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
+
+window.addEventListener("load", () => {
+  langInput.focus();
+});
+
+//? deleteBtn event handler
+
+deleteBtn.addEventListener("click", () => {
+  newUl.removeChild(newUl.lastElementChild);
+});
+
+//? onload event handler
