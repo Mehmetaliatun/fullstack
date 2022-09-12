8eeda4a30e1d750d51e34c79a29b385a9fe0268b
Atun <mali1996ua@gmail.com>
2022-09-12 20:43:56 +0300

    onmouseover

--- Fullstack/JavaScript/Lesson14/1-events-intro/events.js
+++ Fullstack/JavaScript/Lesson14/1-events-intro/events.js
@@ -7,9 +7,12 @@ console.log("****** EVENTS *******");
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
 
+const h1 = document.querySelector("#header h1");
 
-
-
+h1.onmouseover = function () {
+  h1.style.color = "red";
+  h1.style.fontWeight = "300";
+};
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
