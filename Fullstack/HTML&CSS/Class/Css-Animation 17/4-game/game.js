872d43ab9f3d3d5a948601e51bfd142ad73b5e8d
Atun <mali1996ua@gmail.com>
2022-09-15 22:56:20 +0300

    js codes

---
+++ Fullstack/HTML&CSS/Class/Css-Animation 17/4-game/game.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+const car = document.querySelector(".car");
+const ground = document.querySelector(".ground");
+
+body.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+  }
+});
