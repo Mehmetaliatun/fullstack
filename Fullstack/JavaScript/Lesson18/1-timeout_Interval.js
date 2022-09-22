fb20abe39ecb86dc3ad530baa79ae6531e26aeb9
Atun <mali1996ua@gmail.com>
2022-09-22 21:33:23 +0300

    callback

--- Fullstack/JavaScript/Lesson18/1-timeout_Interval.js
+++ Fullstack/JavaScript/Lesson18/1-timeout_Interval.js
@@ -48,3 +48,45 @@
 //     console.log("Nasıl ama :)");
 //   }
 // }, 1000);
+//! geri sayim
+// let counter = 11;
+// const intervalId = setInterval(() => {
+//   console.log(--counter, "Time is going");
+//   if (counter < 1) {
+//     clearInterval(intervalId);
+//     console.log("End of the time!");
+//   }
+// }, 1000);
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+// setTimeout(() => {
+//   console.log("Hello");
+//   setTimeout(() => {
+//     console.log("Hi mate!");
+//   }, 1000);
+// }, 1000);
+
+setTimeout(() => {
+  console.log("john:Hi");
+  setTimeout(() => {
+    console.log("Sarah: Hello");
+    setTimeout(() => {
+      console.log("John: How Are you?");
+      setTimeout(() => {
+        console.log("Sarah:Fine and you?");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+//? COZUMLER:
+//?----------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
