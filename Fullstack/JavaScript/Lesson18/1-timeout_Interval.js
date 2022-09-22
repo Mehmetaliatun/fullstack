fdb5daff441bb560611b511e8a4567dce64b539f
Atun <mali1996ua@gmail.com>
2022-09-22 21:32:38 +0300

    senkron

---
+++ Fullstack/JavaScript/Lesson18/1-timeout_Interval.js
@@ -0,0 +1,32 @@
+//*==================================================
+//*        1- Senkron-Asenkron Programlama
+//*==================================================
+
+//! Javascript, single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? --------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basriyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
+//? kullanilmasi cok onemlidir.
+
+//* Senkron
+//* ------------------------------------------------
+
+// const bekle = (waitingTime) => {
+//   const startTime = new Date().getTime();
+//   while (new Date().getTime() < startTime + waitingTime); //! noktali virgul veya bos suslu parantez while dongusu icin yeterli
+// };
+
+// console.log("Hello");
+// // alert("CW"); //! blocking code
+// console.time("timer");
+// bekle(3000); //! blocking code
+// console.timeEnd("timer");
+// console.log("FS12");
