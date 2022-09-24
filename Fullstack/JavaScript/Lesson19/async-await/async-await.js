ec9dedb08bf985d88ff1676a93335b6488846772
Atun <mali1996ua@gmail.com>
2022-09-24 12:46:03 +0300

    adding news with innerHTML

--- Fullstack/JavaScript/Lesson19/async-await/async-await.js
+++ Fullstack/JavaScript/Lesson19/async-await/async-await.js
@@ -23,7 +23,7 @@
 //! Makro
 
 const getNews = async function () {
-  const API_KEY = "a0d2399e111b4b138fbf4cbe9d5c4be";
+  const API_KEY = "a0d2399e111b4b138fbf4cbe9d5c4be5";
   const url =
     "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
 
@@ -33,12 +33,30 @@ const getNews = async function () {
       throw new Error(`Something went wrong ${res.status}`);
     }
     const data = await res.json();
-    console.log(data.articles);
+    // console.log(data.articles);
+    renderNews(data.articles);
   } catch (error) {
     console.log(error);
   }
 
   //   console.log(res);
 };
-
+const renderNews = (news) => {
+  const newsList = document.getElementById("news-list");
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //!dest.
+    newsList.innerHTML += `
+    
+        <div class="card" style="width: 18rem;">
+        <img src="${urlToImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
+            <a href="${url}" class="btn btn-primary">Details</a>
+        </div>
+    </div>
+    
+    `;
+  });
+};
 window.addEventListener("load", getNews);
