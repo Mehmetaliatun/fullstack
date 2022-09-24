5202617defdb877f7ef6c69cd5fd270b86532361
Atun <mali1996ua@gmail.com>
2022-09-24 13:29:00 +0300

    fetch api

--- Fullstack/JavaScript/Lesson19/flag-app/app.js
+++ Fullstack/JavaScript/Lesson19/flag-app/app.js
@@ -1,3 +1,15 @@
 //*=========================================================
 //*                     FLAG-APP
 //*=========================================================
+
+const fetchCountryByName = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url).then((res) => {
+    if (!res.ok) {
+      renderError(`Something went wrong: ${res.status}`);
+      throw new Error();
+    }
+  });
+};
+
+fetchCountryByName("Turkey");
