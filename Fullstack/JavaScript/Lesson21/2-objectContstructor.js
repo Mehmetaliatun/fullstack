a9c18c4c6d46427ca4108c5833dc1693a2b45718
Atun <mali1996ua@gmail.com>
2022-09-29 20:48:42 +0300

    object constructor

--- Fullstack/JavaScript/Lesson21/2-objectContstructor.js
+++ Fullstack/JavaScript/Lesson21/2-objectContstructor.js
@@ -9,3 +9,26 @@
 //* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
 
 //? Object Constructor
+function Book(title, author, year) {
+  //! object constructor old. icin ilk harf buyuk.(zorunlu degil anlasilirlik icin)
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+}
+
+//? new keyword'u Book Constructor'ini parameterler ile cagirmaktadir.
+//? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
+//? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
+//? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
+//? her bir instance'da hayat bulmus olur.
+
+//?instance
+const book1 = new Book("Kasagi", "Omer Seyfettin", 1920);
+//?instance
+const book2 = new Book("Sinekli Bakkal", "Halide Edip Adivar", 1930);
+
+console.log(book1);
+console.log(book2);
