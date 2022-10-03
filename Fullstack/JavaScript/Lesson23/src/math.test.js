cece9f569b42ce991f6dc67222eabeed9dff8cb1
Atun <mali1996ua@gmail.com>
2022-10-03 22:39:58 +0300

    test end

--- Fullstack/JavaScript/Lesson23/src/math.test.js
+++ Fullstack/JavaScript/Lesson23/src/math.test.js
@@ -26,12 +26,41 @@ describe("calculateTotal", () => {
     expect(calculateTotal(200)).toBe(236);
   });
 });
-describe("celciusToFahrenheit", () => {
-  //*
+describe("celsiusToFahrenheit", () => {
+  it("should exist", () => {
+    expect(celsiusToFahrenheit).toBeDefined();
+  });
+  it("should return a number", () => {
+    expect(celsiusToFahrenheit(10)).toBe(50);
+  });
+  it("should return a number", () => {
+    expect(celsiusToFahrenheit(20)).toBe(68);
+  });
 });
 describe("fahrenheitToCelsius", () => {
-  //*
+  it("should exist", () => {
+    expect(fahrenheitToCelsius).toBeDefined();
+  });
+  it("should convert 32F to 0c", () => {
+    expect(fahrenheitToCelsius(32)).toBe(0);
+  });
 });
 describe("fibonacci", () => {
-  //*
+  it("should exist", () => {
+    expect(fibonacci).toBeDefined();
+  });
+  it("should return a number", () => {
+    expect(fibonacci(1)).toBe(1);
+  });
+  it("should return 55 for 10th index", () => {
+    expect(fibonacci(10)).toBe(55);
+  });
+  it("should throw an error if number is less than 1", () => {
+    expect(() => fibonacci(0)).toThrow(
+      new Error("Must enter a number greater than 0")
+    );
+  });
+  it("should use default parameter if none was given.", () => {
+    expect(fibonacci()).toBe(1);
+  });
 });
