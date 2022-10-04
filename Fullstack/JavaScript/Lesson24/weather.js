c4c52c0ce9d407d9dea04da1354874fc4e7a300c
Atun <mali1996ua@gmail.com>
2022-10-04 20:41:11 +0300

    jquery

---
+++ Fullstack/JavaScript/Lesson24/weather.js
@@ -0,0 +1,14 @@
+const formJS = document.querySelectorAll("form")[0];
+// const formJquery = jQuery("form").eq(0);
+const formJquery = $("form").eq(0);
+const inputJQ = $(".top-banner input").eq(0);
+const msgJQ = $(".top-banner span").eq(0);
+const listJQ = $(".cities").eq(0);
+
+// console.log(formJS);
+// console.log(formJquery);
+console.log(inputJQ);
+
+//* get(index)=> toArray(get()), eq(index)
+
+//* load vs DOMContentLoaded
