fc71fd43e80747f9e57a9f3b7816161de0e250e0
Atun <mali1996ua@gmail.com>
2022-10-08 13:17:33 +0300

    css styling

--- Fullstack/React/rc02_jsx_comp_basicStyling/src/components/Content.jsx
+++ Fullstack/React/rc02_jsx_comp_basicStyling/src/components/Content.jsx
@@ -7,10 +7,19 @@
 //* property isimlerinde camelCase kullanimalidir.
 //* class isimleri icin className kullanilir.
 //* Material UI, Styled Component, Sass, Bootstrap v.b harici kutuphaneler kullanilabilir.
+//? Harici stillendirme dosyasi
+import "./Content.css";
+
 // ? Dahili bir resim eklemek icin import yapmak gereklidir.
 import img2 from "../img/atam.jpg";
 
 const Content = () => {
+  //* JSX alani
+  const imgStyle = {
+    display: "block",
+    width: "300px",
+    margin: "1rem auto",
+  };
   return (
     <div>
       {/* inline style */}
@@ -21,10 +30,18 @@ const Content = () => {
         kütüphanesidir.
       </p>
       <img
+        style={imgStyle}
         src="https://cdn.pixabay.com/photo/2017/02/15/13/40/tulips-2068692__340.jpg"
         alt="img1"
       />
-      <img src="{img2}" alt="img2" />
+      <img style={imgStyle} src={img2} alt="img2" />
+      <p className="par-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quaerat
+        exercitationem repellendus, perspiciatis quo beatae magni distinctio
+        quis ullam mollitia cupiditate ipsa consectetur asperiores? Illum dolore
+        at, quidem nulla consequatur reiciendis cupiditate eius iusto fugit
+        omnis dolorum quis odio ut?
+      </p>
     </div>
   );
 };
