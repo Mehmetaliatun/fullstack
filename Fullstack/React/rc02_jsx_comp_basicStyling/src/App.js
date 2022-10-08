08e1cb1aa234ecbf2a040893cde9ea7c2142b863
Atun <mali1996ua@gmail.com>
2022-10-08 12:43:55 +0300

    css

--- Fullstack/React/rc02_jsx_comp_basicStyling/src/App.js
+++ Fullstack/React/rc02_jsx_comp_basicStyling/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 //? React.JSX kullanmaktadir.
 //? JSX icinde, html elementleri dogrudan js olarak kullanilabilir.
 
-import Header from "./components/Header";
 //! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
 //! bir kod parcacigidir.
 //! Bir componentin return () kismi ise JSX kodlari icerir.
@@ -15,7 +18,13 @@ import Header from "./components/Header";
 //? App componetinin tanimlanmasi
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- Fullstack/React/rc02_jsx_comp_basicStyling/src/components/Footer.jsx
+++ Fullstack/React/rc02_jsx_comp_basicStyling/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 // Componentler fonksiyonlarda oldugu gibi 3 farkli yontemle yazilabilir.
 // JSX icerisinde degiskenler {} icerisinde gosterilir.
 // Footer comp.
-
+// Unutmayalım  =>  { "değişkenler"  ve  "js" kodları süslü içine }
 const Footer = () => {
   return <footer>Copyright by ATUN, {new Date().getFullYear()}</footer>;
 };
+
+export default Footer;
