878c27bd1977ab22488423c515d5bade26361165
Atun <mali1996ua@gmail.com>
2022-10-08 13:28:47 +0300

    props

--- Fullstack/React/rc02_props/src/App.js
+++ Fullstack/React/rc02_props/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Msg from "./components/Msg";
 
 function App() {
   return (
     <div>
       <h1>PROPS</h1>
+      <Msg />
     </div>
   );
 }
---
+++ Fullstack/React/rc02_props/src/components/Msg.jsx
@@ -0,0 +1,9 @@
+const Msg = () => {
+  return (
+    <div>
+      <h3>Merhaba Atun</h3>
+    </div>
+  );
+};
+
+export default Msg;
