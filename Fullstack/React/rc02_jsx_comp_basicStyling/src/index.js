3214ada9f4781cc97762c810822a5a2a0c2f63c6
Atun <mali1996ua@gmail.com>
2022-10-08 11:48:29 +0300

    function return

--- Fullstack/React/rc02_jsx_comp_basicStyling/src/index.js
+++ Fullstack/React/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 import "./index.css";
+const a = 5;
+const element = () => {
+  return (
+    <>
+      <h1>Hello React {a}</h1>
+      <p>Merhaba</p>
+    </>
+  );
+};
+console.log(element);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(element());
