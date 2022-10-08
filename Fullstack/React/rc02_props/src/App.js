9be3ccf275efc945a44c2bbd5d6166a14a75db79
Atun <mali1996ua@gmail.com>
2022-10-08 14:12:19 +0300

    end

--- Fullstack/React/rc02_props/src/App.js
+++ Fullstack/React/rc02_props/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Msg from "./components/Msg";
+// import Msg from "./components/Msg";
 import Person from "./components/Person";
 import data from "./data";
 
@@ -11,9 +11,18 @@ function App() {
       <Msg name="Ali" />
       <Msg name="Atun" /> */}
       {/* {Msg({ name: "Yaren" })} */}
-      <Person name={data[0].name} img={data[0].img} tel={data[0].tel} />
+      {/* <Person name={data[0].name} img={data[0].img} tel={data[0].tel} />
       <Person name={data[1].name} img={data[1].img} tel={data[1].tel} />
-      <Person name={data[2].name} img={data[2].img} tel={data[2].tel} />
+      <Person name={data[2].name} img={data[2].img} tel={data[2].tel} /> */}
+      {/* suslu parantez olursa return yazmak gerekir. */}
+      {data.map((person) => (
+        <Person
+          key={person.name}
+          name={person.name}
+          img={person.img}
+          tel={person.tel}
+        />
+      ))}
     </div>
   );
 }
