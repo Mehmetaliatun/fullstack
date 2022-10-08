b291d36eed0efae61acedd6072fd456451966772
Atun <mali1996ua@gmail.com>
2022-10-08 13:42:24 +0300

    export-import

--- Fullstack/React/rc02_props/src/App.js
+++ Fullstack/React/rc02_props/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Msg from "./components/Msg";
+import Person from "./components/Person";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Msg name="Ali" />
       <Msg name="Atun" />
       {/* {Msg({ name: "Yaren" })} */}
+      <Person />
     </div>
   );
 }
---
+++ Fullstack/React/rc02_props/src/components/Person.jsx
@@ -0,0 +1,14 @@
+const Person = () => {
+  return (
+    <div>
+      <p>Merhaba Atun</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+        alt="img1"
+      />
+      <p>55555555555</p>
+    </div>
+  );
+};
+
+export default Person;
