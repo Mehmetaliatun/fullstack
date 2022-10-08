8faad2b31021dd4d695440e0b934b5f2915b16bc
Atun <mali1996ua@gmail.com>
2022-10-08 11:56:35 +0300

    comp. & func.

--- Fullstack/React/rc02_jsx_comp_basicStyling/src/index.js
+++ Fullstack/React/rc02_jsx_comp_basicStyling/src/index.js
@@ -4,15 +4,19 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import "./index.css";
 const a = 5;
-const element = () => {
+const App = () => {
   return (
     <>
       <h1>Hello React {a}</h1>
       <p>Merhaba</p>
+      <Child />
+      {/* {Child()} */} //! bu sekilde kullanmiyoruz, cunku react istemiyor.
     </>
   );
 };
-console.log(element);
+// console.log(element);
+
+const Child = () => <p>I am not a baby</p>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element());
+root.render(<App />);
