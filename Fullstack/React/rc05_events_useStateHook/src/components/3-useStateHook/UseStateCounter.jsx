c8097e3e60197e2c8fe0a3bc273ef979b7ef4137
Atun <mali1996ua@gmail.com>
2022-10-12 22:59:00 +0300

    hook cont

--- Fullstack/React/rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
+++ Fullstack/React/rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -30,6 +30,13 @@ const UseStateCounter = () => {
 
   const [count, setCount] = useState(0); //? arr destr.
 
+  const [person, setPerson] = useState({
+    name: "Mehmet",
+    surname: "Ali",
+    age: 41,
+  });
+
+  //   console.log(person);
   const inc = () => {
     setCount(count + 1);
   };
@@ -39,27 +46,44 @@ const UseStateCounter = () => {
   //     setCount(count - 1);
   //   }
   // };
-
+  const incAge = () => {
+    //! Bu sekilde atama yapma.
+    //! Bu sekilde atama ile sayisal deger state'in uzerine yazilmis oldu
+    //! Dolayisiyla obje yapisi bozuldu.
+    setPerson(person.age + 1);
+  };
+  console.log(person);
   return (
     <div className="container text-center mt-4">
-      <h1>USESTATE HOOK</h1>
-      <h2 className="display-4 text-danger">COUNT:{count}</h2>
-      <button onClick={inc} className="btn btn-success">
-        INC
-      </button>
+      <section>
+        <h1>USESTATE HOOK</h1>
+        <h2 className="display-4 text-danger">COUNT:{count}</h2>
+        <button onClick={inc} className="btn btn-success">
+          INC
+        </button>
 
-      <button onClick={() => setCount(0)} className="btn btn-dark">
-        CLR
-      </button>
-      {/* <button onClick={dec} className="btn btn-danger">
+        <button onClick={() => setCount(0)} className="btn btn-dark">
+          CLR
+        </button>
+        {/* <button onClick={dec} className="btn btn-danger">
         DEC
       </button> */}
-      <button
-        onClick={() => count > 0 && setCount(count - 1)}
-        className="btn btn-danger"
-      >
-        DEC
-      </button>
+        <button
+          onClick={() => count > 0 && setCount(count - 1)}
+          className="btn btn-danger"
+        >
+          DEC
+        </button>
+      </section>
+      <section>
+        <h1>USESTATE OBJECT</h1>
+        <h2>{person.name}</h2>
+        <h2>{person.surname}</h2>
+        <h4>{person.age}</h4>
+        <button onClick={incAge} className="btn btn-info">
+          inc age
+        </button>
+      </section>
     </div>
   );
 };
