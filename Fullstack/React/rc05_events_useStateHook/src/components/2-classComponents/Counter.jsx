f316ca903c6a4092d03579e984cbbee1309d497a
Atun <mali1996ua@gmail.com>
2022-10-12 21:33:19 +0300

    buttons added

--- Fullstack/React/rc05_events_useStateHook/src/components/2-classComponents/Counter.jsx
+++ Fullstack/React/rc05_events_useStateHook/src/components/2-classComponents/Counter.jsx
@@ -9,8 +9,27 @@
 //* constructor'in disinda state, setState() metodu ile degistilebilir.
 //* ====================================================================================
 
-const Counter = () => {
-  return <div>Counter</div>;
-};
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    //! count state'ine baslangic degeri atadik
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    return (
+      <div className="container text-center mt-4">
+        <h1>Classful Components</h1>
+        <h2 className="display-4 text-danger">Count:</h2>
+        <button className="btn btn-success">INC</button>
+        <button className="btn btn-dark">CLR</button>
+        <button className="btn btn-danger">DEC</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
