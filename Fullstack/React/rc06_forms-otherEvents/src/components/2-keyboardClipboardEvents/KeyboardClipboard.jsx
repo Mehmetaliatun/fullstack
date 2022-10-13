63a0ddcc4813176c808fa802c7fa9f56c58d50ad
Atun <mali1996ua@gmail.com>
2022-10-13 21:36:12 +0300

    keyBoardClipBoard

---
+++ Fullstack/React/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -0,0 +1,10 @@
+const KeyboardClipboard = () => {
+  return (
+    <div className="container text-center">
+      <h1>CLIPBOARD EVENTS</h1>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default KeyboardClipboard;
