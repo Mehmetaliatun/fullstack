9e21c3e6d2f9623e3d783ba4a3200740aaa59b1c
Atun <mali1996ua@gmail.com>
2022-10-13 23:07:58 +0300

    setCoordX,setCoordY

--- Fullstack/React/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ Fullstack/React/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -7,6 +7,9 @@ const MouseEvent = () => {
   const [toggle, setToggle] = useState(false);
   //! cift click atildiginda true-false donuyor
 
+  const [coordX, setCoordX] = useState();
+  const [coordY, setCoordY] = useState();
+
   const handleDoubleClick = (e) => {
     setToggle(!toggle);
     //! classname ile hepsini vermek gerek classlist ile ekleme yapılır
@@ -16,6 +19,14 @@ const MouseEvent = () => {
       : (e.target.className = "bg-success text-light w-50 p-4 mt-4");
   };
   console.log(toggle);
+
+  //! mouseMove event
+  const handleMouseMove = (e) => {
+    setCoordX(e.nativeEvent.offsetX); //!Goreceli
+    setCoordY(e.nativeEvent.offsetY);
+    // setCoordX(e.pageX);
+    // setCoordY(e.pageY);
+  };
 
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
@@ -38,12 +49,18 @@ const MouseEvent = () => {
         todo item 2
       </div>
 
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseMove={handleMouseMove}
+      >
         todo item 3
       </div>
 
       <p>X and Y</p>
-      <p className="text-danger fw-bold">coordX - coordY</p>
+      <p className="text-danger fw-bold">
+        {coordX} - {coordY}
+      </p>
     </div>
   );
 };
