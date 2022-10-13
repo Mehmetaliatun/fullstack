045fcb8c404a391a46c46d30fa2af7649afc0d60
Atun <mali1996ua@gmail.com>
2022-10-13 20:42:40 +0300

    onChange

--- Fullstack/React/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ Fullstack/React/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 const Form = () => {
   const [username, setUsername] = useState("Mehmet");
-
+  const [email, setEmail] = useState("mehmet@ali.co");
+  const [password, setPassword] = useState("");
+  //!useStateSnippet ile otomatik geliyor ve tab ile
+  //!2.degiskeni camelCase yapabiliriz.
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target);
     console.log("Subbmitted");
     alert(`username: ${username}
-    email:,
-    password:
+    email:${email},
+    password:${password}
     `);
   };
   const handleUserName = (e) => {
@@ -23,7 +26,7 @@ const Form = () => {
 
       <div className="mb-3">
         <label htmlFor="username" className="form-label">
-          Username: <span>{username} </span>
+          Username: <span className="text-danger">{username} </span>
         </label>
         <input
           type="text"
@@ -35,15 +38,27 @@ const Form = () => {
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">
-          Email address
+          Email: <span className="text-danger">{email}</span>
         </label>
-        <input type="email" className="form-control" id="email" />
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="password" className="form-label">
           Password
         </label>
-        <input type="password" className="form-control" id="password" />
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
       <button type="submit" className="btn btn-primary">
