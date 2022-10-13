355c840bedead0c245fd007b7fd9dafcdb3176dd
Atun <mali1996ua@gmail.com>
2022-10-13 23:00:59 +0300

    onDoubleClick

--- Fullstack/React/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ Fullstack/React/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -4,6 +4,19 @@ const MouseEvent = () => {
   const [showHint, setShowHint] = useState(false);
   //!boolean degisken olusturuldu, baslangic degeri false verildi.
   //!uzerine gelince tepki verecek
+  const [toggle, setToggle] = useState(false);
+  //! cift click atildiginda true-false donuyor
+
+  const handleDoubleClick = (e) => {
+    setToggle(!toggle);
+    //! classname ile hepsini vermek gerek classlist ile ekleme yapılır
+    //!ternary ile yaptık
+    toggle
+      ? (e.target.className = "bg-danger text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-success text-light w-50 p-4 mt-4");
+  };
+  console.log(toggle);
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
@@ -17,7 +30,11 @@ const MouseEvent = () => {
         todo item 1
       </div>
       {showHint && <p>Hello React</p>}
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
         todo item 2
       </div>
 
