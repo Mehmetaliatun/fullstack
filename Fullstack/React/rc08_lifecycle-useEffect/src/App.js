1f361a595b971904cdce26b4fc4e136fc1812e90
Atun <mali1996ua@gmail.com>
2022-10-17 22:44:00 +0300

    user api start

--- Fullstack/React/rc08_lifecycle-useEffect/src/App.js
+++ Fullstack/React/rc08_lifecycle-useEffect/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
 import UseEffectHook from "./components/useEffectHook";
+import User from "./components/User";
 
 function App() {
   const [show, setShow] = useState(true);
@@ -8,12 +9,13 @@ function App() {
   // console.log(show);
   return (
     <div className="container text-center mt-4">
-      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
         {show ? "Hide" : "Show"}
-      </button>
+      </button> */}
       {/* //! short circuit kullanildi kisaca. */}
       {/* {show && <LifeCycleMethods />} */}
-      {show && <UseEffectHook />}
+      {/* {show && <UseEffectHook />} */}
+      <User />
     </div>
   );
 }
---
+++ Fullstack/React/rc08_lifecycle-useEffect/src/components/User.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <h1>NAME</h1>
+      <img src="" alt="" />
+      <p>email</p>
+      <h5>DOB</h5>
+    </div>
+  );
+};
+
+export default User;
