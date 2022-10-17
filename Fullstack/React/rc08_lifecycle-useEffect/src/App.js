c0eac4e06954289d13359da72c8d1c2475258e64
Atun <mali1996ua@gmail.com>
2022-10-17 21:22:09 +0300

    addBtn

--- Fullstack/React/rc08_lifecycle-useEffect/src/App.js
+++ Fullstack/React/rc08_lifecycle-useEffect/src/App.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
 
 function App() {
+  const [show, setShow] = useState(true);
+  //! ctrl+space ile import edebiliriz.
   return (
     <div className="container text-center mt-4">
+      <button className="btn btn-dark" onClick={() => setShow(!show)}>
+        Show/Hide
+      </button>
       <LifeCycleMethods />
     </div>
   );
