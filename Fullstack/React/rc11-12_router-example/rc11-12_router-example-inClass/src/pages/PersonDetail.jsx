150c3d78b37562dfb7da7f9d7985fde9328eaf35
Atun <mali1996ua@gmail.com>
2022-10-22 14:05:49 +0300

    fetch,then,catch

--- Fullstack/React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ Fullstack/React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
   const navigate = useNavigate();
-
+  //! Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
   const { id } = useParams();
   console.log(id);
 
-  const { state: person } = useLocation();
+  // navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
+  // Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz.
+  //   const { state: person } = useLocation();
+  //   console.log(person);
   //   ! gonderirken state fakat burada bu sekilde yeni bir atama yapilabilir.
+
+  const [person, setPerson] = useState("");
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPerson(data.data))
+      .catch((err) => console.log(err));
+  }, []);
   console.log(person);
+
   return (
     <div className="container text-center">
       <h3>
-        {person.first_name} {person.last_name}
+        {person?.first_name} {person?.last_name}
       </h3>
-      <img className="rounded-circle" src={person.avatar} alt="" />
+      <img className="rounded-circle" src={person?.avatar} alt="" />
 
-      <p>{person.email}</p>
+      <p>{person?.email}</p>
       <div>
         <button onClick={() => navigate("/")} className="btn btn-success me-2">
           Go Home
