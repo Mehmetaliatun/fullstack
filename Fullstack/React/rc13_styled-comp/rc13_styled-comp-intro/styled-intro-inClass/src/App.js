9d5857319bb5dc4322ac4731f3d56d2b99e3106f
Atun <mali1996ua@gmail.com>
2022-10-25 21:36:20 +0300

    css components

--- Fullstack/React/rc13_styled-comp/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ Fullstack/React/rc13_styled-comp/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,14 +1,25 @@
 import { Button, TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
+import HeaderText from "./components/HeaderText.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
     <>
       <Container bg="gray">
+        <HeaderText color="hotpink">Styled Component</HeaderText>
         <Button primary>Save</Button>
         <Button>Clear</Button>
         <TomatoButton primary>Submit</TomatoButton>
         <TomatoButton>Send</TomatoButton>
+      </Container>
+      <Container bg="#eee">
+        <StyledLink href="https://www.google.com.tr" target="_blank">
+          Google
+        </StyledLink>
+        <StyledLink secondary href="https://www.reactjs.org/" target="_blank">
+          React
+        </StyledLink>
       </Container>
     </>
   );
--- Fullstack/React/rc13_styled-comp/rc13_styled-comp-intro/styled-intro-inClass/src/components/Container.styled.jsx
+++ Fullstack/React/rc13_styled-comp/rc13_styled-comp-intro/styled-intro-inClass/src/components/Container.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Container = styled.div`
   background-color: ${({ bg }) => bg || "white"};
@@ -7,6 +7,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 0.5rem;
+  padding: 1rem;
 `;
 
 export default Container;
