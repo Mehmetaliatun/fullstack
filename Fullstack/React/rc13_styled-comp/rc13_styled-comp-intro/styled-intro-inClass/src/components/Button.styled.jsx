820df9504dbde80056c032f91bade8f7f13ebac3
Atun <mali1996ua@gmail.com>
2022-10-25 20:31:20 +0300

    style starting

---
+++ Fullstack/React/rc13_styled-comp/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+//! component olustur, style verecegimiz icin styled yaz,
+//! button olmasini istedigimiz icin adini ona gore veriyoruz.
+//! daha sonra back tick ile yazilan alani dolduruyoruz.
+const Button = styled.button`
+  background-color: hotpink;
+  color: white;
+`;
