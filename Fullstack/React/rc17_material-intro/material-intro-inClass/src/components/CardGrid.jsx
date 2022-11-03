52f93da15781347b0e5cf65b5ae681910ce0a4cd
Atun <mali1996ua@gmail.com>
2022-11-03 21:59:26 +0300

    card&grid

--- Fullstack/React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
+++ Fullstack/React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
@@ -6,32 +6,38 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import data from "../data";
+import Grid from "@mui/material/Grid";
 
 export default function CardGrid() {
   return (
     <>
-      {data.map((card) => {
-        const { id, text, img, name } = card;
-        return (
-          <div>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia component="img" image={img} alt="img" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {text}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </div>
-        );
-      })}
+      <Typography variant="h4" color="error" align="center" mt={4} mb={4}>
+        Card And Grid
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {data.map((card) => {
+          const { id, text, img, name } = card;
+          return (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia component="img" image={img} alt="img" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {text}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </>
   );
 }
