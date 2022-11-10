bd6103e0cdc09ceda42934e4edaeea1e13de4861
Atun <mali1996ua@gmail.com>
2022-11-10 22:31:39 +0300

    combineReducers

--- Fullstack/React/rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
+++ Fullstack/React/rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
@@ -1,8 +1,11 @@
 import "./Counter.css";
 import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../../redux/actions/counterAction";
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.count.counter);
+  //! ortak yapmadan onceki hali
+  // const counter = useSelector((state) => state.counter);
   // const {counter} = useSelector((state) => state);
   //! ustteki gibi de kullanilabilir.
   return (
@@ -12,19 +15,20 @@ const Counter = () => {
       <div>
         <button
           className="counter-button positive"
-          onClick={() => dispatch({ type: "INCREMENT" })}
+          onClick={() => dispatch(increment())}
+          // onClick={() => dispatch(increment())}
         >
           increase
         </button>
         <button
           className="counter-button zero"
-          onClick={() => dispatch({ type: "RESET" })}
+          onClick={() => dispatch(reset())}
         >
           reset
         </button>
         <button
           className="counter-button negative"
-          onClick={() => dispatch({ type: "DECREMENT" })}
+          onClick={() => dispatch(decrement())}
         >
           decrease
         </button>
