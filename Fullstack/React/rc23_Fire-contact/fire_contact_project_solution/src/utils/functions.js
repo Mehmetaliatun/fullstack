7a2e70e8196c87c00344543df4a438457b07a1ea
Atun <mali1996ua@gmail.com>
2022-11-14 23:40:48 +0300

    useFetch

--- Fullstack/React/rc23_Fire-contact/fire_contact_project_solution/src/utils/functions.js
+++ Fullstack/React/rc23_Fire-contact/fire_contact_project_solution/src/utils/functions.js
@@ -1,55 +1,60 @@
-import firebase from "./firebase"
-import { getDatabase,onValue,push, ref, remove, set, update  } from "firebase/database";
-import {useState,useEffect} from "react"
-import Toastify from "./toastify"
-
+import firebase from "./firebase";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+  update,
+} from "firebase/database";
+import { useState, useEffect } from "react";
+import Toastify from "./toastify";
 
 // Bilgi Ekleme
-export const AddUser=(info)=>{
-    const db = getDatabase(firebase);
-    const userRef=ref(db,"users/")
-    const newUserRef=push(userRef);
-    set(newUserRef,{
-        username:info.username,
-        phoneNumber:info.phoneNumber,
-        gender:info.gender
-    })
-
-}
+export const AddUser = (info) => {
+  const db = getDatabase(firebase);
+  const userRef = ref(db, "users/");
+  const newUserRef = push(userRef);
+  set(newUserRef, {
+    username: info.username,
+    phoneNumber: info.phoneNumber,
+    gender: info.gender,
+  });
+};
 
 // Bilgi Çağırma
 
-export const useFetch=()=>{
-     const [isLoading,setIsLoading]=useState();
-     const [contactList,setContactList]=useState();
-    useEffect(() => {
-        const db = getDatabase(firebase);
-        const userRef=ref(db,"users/")
-        onValue(userRef,(snapshot)=>{
-            const data=snapshot.val();
-            const userArray=[]
-
-            for (let id in data){
-                userArray.push({id,...data[id]})
-            }
-            setContactList(userArray)
-            setIsLoading(false)
-        })
-    },[])
-    return {isLoading,contactList}
-}
-
-export const DeleteUser=(id)=>{
+export const useFetch = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [contactList, setContactList] = useState();
+  useEffect(() => {
     const db = getDatabase(firebase);
-    remove(ref(db,"users/"+id));
-    Toastify("Deleted Successfully")
-}
+    const userRef = ref(db, "users/");
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      const userArray = [];
 
-export const UpdateUser=(info)=>{
-    const db = getDatabase(firebase);
-    const updates={}
-    updates["users/"+info.id]=info
+      for (let id in data) {
+        userArray.push({ id, ...data[id] });
+      }
+      setContactList(userArray);
+      setIsLoading(false);
+    });
+  }, []);
+  return { isLoading, contactList };
+};
 
-    return update(ref(db),updates)
+export const DeleteUser = (id) => {
+  const db = getDatabase(firebase);
+  remove(ref(db, "users/" + id));
+  Toastify("Deleted Successfully");
+};
 
-}
+export const UpdateUser = (info) => {
+  const db = getDatabase(firebase);
+  const updates = {};
+  updates["users/" + info.id] = info;
+
+  return update(ref(db), updates);
+};
