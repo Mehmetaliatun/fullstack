4d961e730a2799ce5d9df537e5e4efdb266289f9
Atun <mali1996ua@gmail.com>
2022-11-21 22:20:31 +0300

    sidebar-design and router

--- Fullstack/React/rc25_stock-app/stock-app/src/hooks/useAuthCalls.jsx
+++ Fullstack/React/rc25_stock-app/stock-app/src/hooks/useAuthCalls.jsx
@@ -15,7 +15,7 @@ const useAuthCalls = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const BASE_URL = "https://14241.fullstack.clarusway.com";
+  const BASE_URL = "https://14241.fullstack.clarusway.com/";
 
   const login = async (userInfo) => {
     dispatch(fetchStart());
