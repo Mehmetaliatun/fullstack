84581518d4d4ffbf7ee59a3b58408a673f7691c8
Atun <mali1996ua@gmail.com>
2022-11-28 21:41:43 +0300

    data types

--- Fullstack/TypeScript/ts01-intro-basics/src/index.ts
+++ Fullstack/TypeScript/ts01-intro-basics/src/index.ts
@@ -1,2 +1,27 @@
 export {};
 console.log("Hello TypeScript!");
+// let a = 5;
+let a = 6;
+let b = "Danke";
+console.log(a);
+
+let arr: any[] = ["john", 619, true];
+console.log(arr);
+
+let myVar: any = "maa";
+myVar = 66;
+console.log(myVar);
+
+let numbers: number[] = [];
+// numbers.push("3");
+
+let v1: unknown = 3;
+let v2: number = v1 as number;
+console.log(v1, v2);
+
+let firstName = "enver";
+let lastName = "pasa";
+function getFullName(): void {
+  return; // firstName + " " + lastName;
+}
+// console.log(getFullName().toLocaleUpperCase());
